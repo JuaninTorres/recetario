999d1340fb7d012c419e8b835979c0974f690cdd
Juan Torres <juanin.torres@gmail.com>
2013-02-16 17:05:58 -0300

    siguen los cambios para el paso a produccion

--- recetario/recetario/settings.py
+++ recetario/recetario/settings.py
@@ -18,7 +18,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         #'NAME': RUTA_PROYECTO + '/recetario.db',                      # Para Linux
-        'NAME': 'recetario.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/ec2-user/django/recetario/recetario/recetario.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -56,7 +56,7 @@ MEDIA_ROOT = os.path.join(RUTA_PROYECTO, 'carga')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://django.juanintorres.cl/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -66,7 +66,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://django.juanintorres.cl/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
