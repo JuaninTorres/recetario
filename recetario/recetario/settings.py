f253348a178b8a6797ff720cfe679189a3bfc997
Juan Torres <juanin.torres@gmail.com>
2013-02-17 04:02:52 -0300

    Cambiando a postgres

--- recetario/recetario/settings.py
+++ recetario/recetario/settings.py
@@ -16,13 +16,14 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         #'NAME': RUTA_PROYECTO + '/recetario.db',                      # Para Linux
-        'NAME': '/home/ec2-user/django/recetario/recetario/recetario.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        #'NAME': '/home/ec2-user/django/recetario/recetario/recetario.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'recetariodb',
+        'USER': 'djangouser',                      # Not used with sqlite3.
+        'PASSWORD': 'djangopass',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
